9d9959fcb637c22f9992a8f68a68de48adb0ad54
mynameisname3033 <akhilenj11@gmail.com>
2026-06-27 12:27:38 -0400

    cleanup3

--- piece.h
+++ piece.h
@@ -6,7 +6,6 @@ enum color { WHITE, BLACK, COLOR_NB };
 enum piece { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, PIECE_NB };
 
 inline constexpr char piece_to_char[PIECE_NB] = { 'P','N','B','R','Q','K' };
-int char_to_piece(char piece_char);
 
 __forceinline uint8_t create_full_piece(int color, int piece) { return (color << 3) | piece; }
 __forceinline int full_piece_color(uint8_t full_piece) { return full_piece >> 3; }
